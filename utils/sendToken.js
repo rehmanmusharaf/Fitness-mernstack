f3044eb235852dcb2104933346378ebc78418063
Muhammad Rehman <muhammadrehmanmusharafali@gmail.com>
2024-06-10 03:01:14 +0500

    Send Token Make ready ofr Production

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -1,48 +1,21 @@
-const sendToken = async (user, statuscode, res) => {
-  try {
-    // console.log("Send Token function run!");
-    const token = await user.getJwtToken();
-
-    // console.log("token is  : ", token);
-    const option = {
-      expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
-      httpOnly: true,
-    };
-    // user = Object.keys(user)
-    //   .filter((objKey) => objKey !== "password")
-    //   .reduce((newObj, key) => {
-    //     newObj[key] = user[key];
-    //     return newObj;
-    //   }, {});
-    delete user.password;
-    // console.log("after deleting user password user is", user);
-    res
-      .status(statuscode)
-      .cookie("token", token, option)
-      .json({ success: true, user, token });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: "Error During token genaration",
-      error: error.message,
-    });
-  }
-};
-
-module.exports = sendToken;
 // const sendToken = async (user, statuscode, res) => {
 //   try {
+//     // console.log("Send Token function run!");
 //     const token = await user.getJwtToken();
 
+//     // console.log("token is  : ", token);
 //     const option = {
 //       expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
 //       httpOnly: true,
-//       // secure: process.env.NODE_ENV === "production", // Set secure flag to true in production
-//       // sameSite: "None", // Necessary for cross-site requests
 //     };
-
+//     // user = Object.keys(user)
+//     //   .filter((objKey) => objKey !== "password")
+//     //   .reduce((newObj, key) => {
+//     //     newObj[key] = user[key];
+//     //     return newObj;
+//     //   }, {});
 //     delete user.password;
-
+//     // console.log("after deleting user password user is", user);
 //     res
 //       .status(statuscode)
 //       .cookie("token", token, option)
@@ -50,10 +23,37 @@ module.exports = sendToken;
 //   } catch (error) {
 //     res.status(500).json({
 //       success: false,
-//       message: "Error During token generation",
+//       message: "Error During token genaration",
 //       error: error.message,
 //     });
 //   }
 // };
 
 // module.exports = sendToken;
+const sendToken = async (user, statuscode, res) => {
+  try {
+    const token = await user.getJwtToken();
+
+    const option = {
+      expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production", // Set secure flag to true in production
+      sameSite: "None", // Necessary for cross-site requests
+    };
+
+    delete user.password;
+
+    res
+      .status(statuscode)
+      .cookie("token", token, option)
+      .json({ success: true, user, token });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error During token generation",
+      error: error.message,
+    });
+  }
+};
+
+module.exports = sendToken;
